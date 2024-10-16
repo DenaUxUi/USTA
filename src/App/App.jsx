2d65a80223b10bf83580cb50c6f82de51b56775e
Dena_ <155665379+DenaUxUi@users.noreply.github.com>
2024-10-16 16:22:43 +0600

    Added Routes for Pages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,32 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../Shared/Components/Header/Header';
+import Footer from '../Shared/Components/Footer/Footer';
+import Home from '../Pages/Home/Home';
+import Ambassadorship from '../Pages/Ambassadorship/Ambassadorship';
+import Community from '../Pages/Community/Community';
+import Education from '../Pages/Education/Education';
+import Events from '../Pages/Events/Events';
+import Membership from '../Pages/Membership/Membership';
+import Participation from '../Pages/Participation/Participation';
+import Services from '../Pages/Services/Services';
 
 function App() {
   
   return (
-    <>
-      PlaceHolde
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route to="/" element={<Home/>}/>
+        <Route to="/ambassadorship" element={<Ambassadorship/>}/>
+        <Route to="/community" element={<Community/>}/>
+        <Route to="/education" element={<Education/>}/>
+        <Route to="/events" element={<Events />}/>
+        <Route to="/membership" element={<Membership />}/>
+        <Route to="/participation" element={<Participation />}/>
+        <Route to="services" element={<Services />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
